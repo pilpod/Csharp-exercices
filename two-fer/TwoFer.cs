515286dc80393044b3956c3d2d6284d4fa9f80b5
pilpod <giacomo.corso@gmail.com>
2021-03-14 16:51:04 +0100

    exercice finished

---
+++ two-fer/TwoFer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(string name = null)
+    {
+        if(name == null) {
+            return "One for you, one for me.";
+        }
+
+        return $"One for {name}, one for me.";
+
+    }
+
+}
