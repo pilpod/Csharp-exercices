b3bf8821b3680a9c44987b6b0a56c8a769693e28
pilpod <giacomo.corso@gmail.com>
2021-03-14 18:10:53 +0100

    exercice finished

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static string[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    public static int ColorCode(string color)
+    {
+
+        int number = Array.IndexOf(colors, color);
+        return number;
+    }
+
+    public static string[] Colors()
+    {   
+        return colors;
+    }
+}
+
