6209b9ccfc7bee6d2d90a64a7bd963c18c15c19c
pilpod <giacomo.corso@gmail.com>
2021-04-25 17:23:12 +0200

    pending last three test and refactoring

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int count;
+        List<int> numbList = new List<int>();
+
+        foreach (var item in multiples)
+        {
+            count = 1;
+            for (int i = 0; i < max; i++)
+            {
+                if(count % item == 0 && !numbList.Contains(count) && count < max) {
+                    numbList.Add(count);
+                }
+                count++;
+            }
+        }
+        
+        for (int i = 0; i < numbList.Count; i++)
+        {
+            Console.WriteLine(numbList[i]);
+        }
+
+        int result = numbList.Sum();
+
+        return result;
+    }
+}
