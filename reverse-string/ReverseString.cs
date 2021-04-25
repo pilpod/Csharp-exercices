70d0dd7b32bba98768eb0567058dcd62e0b211a0
pilpod <giacomo.corso@gmail.com>
2021-04-25 15:28:01 +0200

    new simple exercice finished - reverse string

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] output = input.ToCharArray();
+        Array.Reverse( output );
+        string revertedString = string.Join("", output);
+        return revertedString;
+    }
+}
